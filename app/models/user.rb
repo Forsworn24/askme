7e8a5d793fc05540fce6532ffce4c724ab7d2f72
forsworn24 <mudinov.ali2401@gmail.com>
2022-08-12 02:58:25 +0500

    fix input data of questions and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
+  # эта запись дополнительно добавляет валидацию
   has_secure_password
+
+  before_save :downcase_nickname
+
+  /^[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+$/i
+
+  validates :email, presence: true, uniqueness: true, 
+    format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\Z/i,
+    message: "Invalid email format" }
+  
+  validates :nickname, presence: true, uniqueness: true, length: { maximum:40 },
+    format: { with: /\A[0-9a-zA-Z\_]+\Z/, message: "Only latin letters, numbers and _ sign" }
+    
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
