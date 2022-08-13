6aeef7aa36ee2a2669f78905570c958a0005f680
forsworn24 <mudinov.ali2401@gmail.com>
2022-08-13 07:34:15 +0500

    fix br

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,11 @@ class User < ApplicationRecord
 
   before_save :downcase_nickname
 
-  /^[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+$/i
-
   validates :email, presence: true, uniqueness: true, 
-    format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\Z/i,
-    message: "Invalid email format" }
+    format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\Z/i }
   
   validates :nickname, presence: true, uniqueness: true, length: { maximum:40 },
-    format: { with: /\A[0-9a-zA-Z\_]+\Z/, message: "Only latin letters, numbers and _ sign" }
+    format: { with: /\A[0-9a-zA-Z\_]+\Z/ }
     
   def downcase_nickname
     nickname.downcase!
