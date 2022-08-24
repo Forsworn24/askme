e8c2c714026865037e2673cc99a8a0fca07cb9ad
Forsworn24 <mudinov.ali2401@gmail.com>
2022-08-24 22:32:42 +0500

    delete gem friendly and comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,7 @@ Rails.application.routes.draw do
   resource :session, only: %i[new create destroy]
   resources :users, param: :nickname, except: %i[index]
 
-  # get '/users/:nickname', to: 'users#show'
-
   resources :questions do
     get :hide, on: :member
   end
-
-  # resources :questions do
-  #   member do
-  #     get :hide
-  #   end
-  # end 
 end
