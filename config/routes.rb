93effbdab8551bbc97febb976a2e5112b6e26fd6
Forsworn24 <mudinov.ali2401@gmail.com>
2022-09-05 17:49:42 +0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
   
-  resources :questions
+  resources :questions, except: [:show, :new, :index]
   resource :session, only: %i[new create destroy]
   resources :users, param: :nickname, except: %i[index]
   resources :hashtags, param: :name, only: %i[show]
