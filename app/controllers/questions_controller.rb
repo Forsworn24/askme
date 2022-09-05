e7e9f7ce13cfd96be49d3fbc1cf999587c1f9974
Forsworn24 <mudinov.ali2401@gmail.com>
2022-09-05 22:45:21 +0500

    initial commit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,7 +8,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.author = current_user
     
-    if @question.save
+    if check_captcha(@question) && @question.save
       redirect_to user_path(@question.user), notice: "Новый вопрос создан!"
     else
       flash.now[:alert] = "Вы неправильно заполнили поля формы вопроса, количество символов не должно превышать 280"
@@ -69,5 +69,13 @@ class QuestionsController < ApplicationController
 
   def set_question_for_current_user
     @question = current_user.questions.find(params[:id])
+  end
+
+  def check_captcha(model)
+    if current_user.present?
+      true
+    else
+      verify_recaptcha(model: model)
+    end
   end
 end
