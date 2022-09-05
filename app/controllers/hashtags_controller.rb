3d08c87e67eacde17bb8a452fd7f98939fdd62ea
Forsworn24 <mudinov.ali2401@gmail.com>
2022-09-05 16:17:38 +0500

    fix-mistakes in models and controllers

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -2,12 +2,6 @@ class HashtagsController < ApplicationController
   def show
     @hashtag = Hashtag.find_by(name: params[:name])
 
-    if !@hashtag.present?
-      redirect_to root_path, notice: "Такого хэштега не существует"
-      return
-    end
-
-    @hq = HashtagQuestion.where(hashtag_id: @hashtag.id)
-    @questions = Question.where(id: @hq.pluck(:question_id))
+    redirect_to root_path, notice: "Такого хэштега не существует" if @hashtag.blank?
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -7,24 +7,13 @@ class Question < ApplicationRecord
   has_many :hashtag_questions, dependent: :destroy
   has_many :hashtags, through: :hashtag_questions
 
-  # after_save_commit - замена нижней строки(аналог)
   after_commit :create_hashtags, on: %i[create update]
 
   def create_hashtags
-
-    HashtagQuestion.where(question_id: id).destroy_all
-
-    extract_hashtags.each do |body|
-      hashtags << Hashtag.find_or_create_by(name: body)
-    end
+    self.hashtags = extract_hashtags.map { |hashtag_name| Hashtag.find_or_create_by(name: hashtag_name) }
   end
 
   def extract_hashtags
-    "#{body} #{answer}".downcase.scan(/#[[:word:]]+/).map{ |text| text.gsub("#", "") }
+    "#{body} #{answer}".downcase.scan(/#[[:word:]]+/).map{ |text| text.gsub("#", "") }.uniq
   end
-
-  # def to_param
-  #   name
-  # end
-
 end
